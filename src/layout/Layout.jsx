fa5884a77b8fa349597a0ffe841f6593c19fc56d
João Jamba <joao.jamba@ideiasdinamicas.com>
2024-05-07 02:07:02 +0100

    layout center fixed

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,18 +5,23 @@ import { Outlet } from "react-router-dom";
 
 function Layout({ Children }) {
   return (
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div
-          className={`${styles.paddingX} ${styles.flexCenter} z-50 fixed w-full top-0 left-0`}
-        >
-          <div className={`${styles.boxWidth}`}>
-            <Navbar />
-          </div>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
         </div>
-        {Children}
-      <main>
-        <Outlet />
-      </main>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>{Children}</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
